302f9a8dec0dc1415b300b6ddf63aef5ee8de7d1
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-01-19 16:17:13 -0300

    💄feat: global style configs

---
+++ src/style/theme/default.ts
@@ -0,0 +1,29 @@
+export const defaultTheme = {
+  colors: {
+    blue: '#3294F8',
+    'base-title': '#E7EDF4',
+    'base-subtitle': '#C4D4E3',
+    'base-text': '#AFC2D4',
+    'base-span': '#7B96B2',
+    'base-label': '#3A536B',
+    'base-border': '#1C2F41',
+    'base-post': '#112131',
+    'base-profile': '#0B1B2B',
+    'base-background': '#071422',
+    'base-input': '#040F1A'
+  },
+  textSizes: {
+    'title-title-x': '1.5rem',
+    'text-regular-l': '1.25rem',
+    'text-bold-l': '1.25rem',
+    'text-regular-m': '1rem',
+    'text-bold-m': '1rem',
+    'text-regular-s': '0.875rem',
+    'text-bold-s': '0.75rem',
+    'components-button-g': '0.875rem',
+    'components-button-s': '0.75rem'
+  },
+  fonts: {
+    Nunito: 'Nunito'
+  }
+} as const
