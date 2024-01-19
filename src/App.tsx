dbab6d19702740563c455bd71b1819edd667287f
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2024-01-19 16:18:18 -0300

    ✨feat: app file config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './style/theme/default'
+import { GlobalStyle } from './style/global'
+import { Blog } from './page/Blog'
+
 export function App() {
   return (
-    <>
-      <h1> Hello, World!</h1>
-      <h2>First commit!</h2>
-    </>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <Blog />
+    </ThemeProvider>
   )
 }
